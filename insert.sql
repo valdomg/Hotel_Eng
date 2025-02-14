22230f4afb9f332de8022c4aa3789bca741d22f2
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-02-13 22:11:51 -0300

    Insert de testes

---
+++ insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO hoteis (nome, localizacao, descricao, categoria, hotel_img) VALUES
+('Hotel Luxor', 'Rio de Janeiro, RJ', 'Um hotel de luxo com vista para o mar.', 'luxo', 1),
+('Pousada das Montanhas', 'Campos do Jordão, SP', 'Charmosa pousada nas montanhas.', 'pousada',2),
+('Resort Paraíso', 'Porto de Galinhas, PE', 'Resort all-inclusive com piscinas naturais.', 'resort',3),
+('Hotel Econômico Centro', 'São Paulo, SP', 'Opção acessível no centro da cidade.', 'econômico',4);
+
+INSERT INTO hotel_imagens(hotel_id, caminho) values
+(1 ,'imagens/hoteis/1.png');
+
+
+INSERT INTO hotel_imagens(hotel_id, caminho) values
+(2 ,'imagens/hoteis/1.png');
+
+
+INSERT INTO hotel_imagens(hotel_id, caminho) values
+(3 ,'imagens/hoteis/1.png');
+
+
+INSERT INTO hotel_imagens(hotel_id, caminho) values
+(4 ,'imagens/hoteis/1.png');
