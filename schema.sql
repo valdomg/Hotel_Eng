43a3897a3c9020d9daa8fed9a1293d149e89ea07
Valdemiro Gabriel <valdemiro128@outlook.com>
2025-02-25 22:33:46 -0300

    Novo Schema de Banco de Dados

--- schema.sql
+++ schema.sql
@@ -1,14 +1,20 @@
-CREATE DATABASE IF NOT EXISTS hotel_management;
-USE hotel_management;
-#drop database hotel_management;
+CREATE DATABASE IF NOT EXISTS hotel_teste;
+USE hotel_teste;
+#drop database hotel_teste;
 
 -- Tabela de Usuários
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    senha VARCHAR(255) NOT NULL,
-    tipo ENUM('admin', 'funcionario') NOT NULL
+    senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE admins(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL
 );
 
 -- Tabela de Hotéis
@@ -16,56 +22,63 @@ CREATE TABLE hoteis (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     localizacao VARCHAR(255) NOT NULL,
-    hotel_img integer,
-    descricao TEXT,
-    categoria ENUM('econômico', 'luxo', 'resort', 'pousada') NOT NULL
+    preco_diaria DECIMAL(5,2),
+    
+    hotel_img_home VARCHAR (255) ,
+    hotel_img_home_patrocinado VARCHAR (255) ,
+    hotel_img_page_hotel VARCHAR(255) ,
+    hotel_img_quarto VARCHAR(255) ,
+    hotel_img_area_lazer VARCHAR(255) ,
+    
+    descricao TEXT NOT NULL,
+    categoria ENUM('Hotel econômico', 'Hotel de luxo', 'Hotel resort', 'Hotel pousada') NOT NULL
 );
 
--- Tabela de Imagens de Hotéis
-CREATE TABLE hotel_imagens (
+-- Tabela de Residências
+CREATE TABLE residencia (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    localizacao VARCHAR(255) NOT NULL,
+    preco_diaria DECIMAL(5,2),
+    
+    residencia_img_home VARCHAR (255) ,
+    residencia_img_home_patrocinado VARCHAR (255) ,
+    residencia_img_quarto VARCHAR(255) ,
+    residencia_img_fachada VARCHAR(255) ,
+    residencia_img_area_lazer VARCHAR(255) ,
+    
+    descricao TEXT NOT NULL,
+    categoria ENUM('Residência na praia', 'Sobrado', 'Bangalô', 'Apartamento', 'Flat') NOT NULL
+);
+
+CREATE TABLE reserva_hotel(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
     hotel_id INT NOT NULL,
-    caminho VARCHAR(255) NOT NULL,
-    FOREIGN KEY (hotel_id) REFERENCES hoteis(id) ON DELETE CASCADE
-);
-
--- Tabela de Quartos
-CREATE TABLE quartos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    hotel_id INT NOT NULL,
-    numero VARCHAR(10) NOT NULL,
-    tipo ENUM('simples', 'duplo', 'suíte') NOT NULL,
-    preco_diaria DECIMAL(10,2) NOT NULL,
-    status ENUM('disponivel', 'ocupado', 'manutencao') NOT NULL DEFAULT 'disponivel',
-    FOREIGN KEY (hotel_id) REFERENCES hoteis(id) ON DELETE CASCADE
-);
-
--- Tabela de Imagens de Quartos
-CREATE TABLE quarto_imagens (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    quarto_id INT NOT NULL,
-    caminho VARCHAR(255) NOT NULL,
-    FOREIGN KEY (quarto_id) REFERENCES quartos(id) ON DELETE CASCADE
-);
-
--- Tabela de Clientes
-CREATE TABLE clientes (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    telefone VARCHAR(20),
-    email VARCHAR(100) UNIQUE NOT NULL,
-    documento VARCHAR(50) UNIQUE NOT NULL
-);
-
--- Tabela de Reservas
-CREATE TABLE reservas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    cliente_id INT NOT NULL,
-    quarto_id INT NOT NULL,
     data_checkin DATE NOT NULL,
     data_checkout DATE NOT NULL,
+    preco_total DECIMAL(5,2),
     status ENUM('ativa', 'finalizada', 'cancelada') NOT NULL DEFAULT 'ativa',
     valor_total DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE,
-    FOREIGN KEY (quarto_id) REFERENCES quartos(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (hotel_id) REFERENCES hoteis(id) ON DELETE CASCADE
 );
+
+CREATE TABLE reserva_residencia(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    residencia_id INT NOT NULL,
+    data_checkin DATE NOT NULL,
+    data_checkout DATE NOT NULL,
+    preco_total DECIMAL(5,2),
+    status ENUM('ativa', 'finalizada', 'cancelada') NOT NULL DEFAULT 'ativa',
+    valor_total DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (residencia_id) REFERENCES residencia(id) ON DELETE CASCADE
+);
+
+
+
+
+
+
