544cf2a336a207983782ead4ec0d36162d5ed0a4
Paulo Braga <uchoapaulo42@gmail.com>
2025-02-27 15:04:09 -0300

    ATUALIZANDO ARQUIVOS

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ MYSQL_CONNECTION = mysql.connector.connect(
     host= os.getenv('DB_HOST'),
     user= os.getenv('DB_USER'),
     password= os.getenv('DB_PASSWORD'),
-    database= os.getenv('DB_DATABASE')
+    database= os.getenv('DB_NAME')
 
 )
 
